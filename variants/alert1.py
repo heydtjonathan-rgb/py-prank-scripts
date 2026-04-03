4917df90ad34f467ade16e4ebb95481217f0ed20
JonisCode-rgb <heydtjonathan@gmail.com>
2026-04-03 11:26:53 +0200

    Modify alert configuration for hacking scenario

--- variants/alert1.py
+++ variants/alert1.py
@@ -6,20 +6,20 @@ import os
 import sys
 
 # ─── ANPASSBAR (KONFIGURATION) ───────────────────────
-DAUER_SEKUNDEN = 120       # Zeit bis zum automatischen Schließen
-TITEL_FENSTER = "SYSTEM ALERT"
-HAUPTTEXT = "ZUGRIFF VERWEIGERT"
-UNTERTEXT = "Sicherheits-Protokoll aktiv..."
+DAUER_SEKUNDEN = 9999999999999       # Zeit bis zum automatischen Schließen
+TITEL_OBEN = "SYSTEM HACKED"
+HAUPTTEXT = " SYSTEM HACKED! ACCESS DENIED!"
+UNTERTEXT = "Pay 100k EUR to get your PC back!"
 HINTERGRUND = "black"
 TEXTFARBE = "red"
 INFOTEXT_FARBE = "white"
-TIMER_FARBE = "gray"
+TIMER_FARBE = "black"
 
-# Selbstlöschung der Datei nach Beenden (True = Ja, False = Nein)
+# Selbstlöschung aktivieren? (True = Ja, False = Nein)
 AUTO_DELETE = True 
 
-# Versteckter Button Einstellungen (unten rechts)
-BUTTON_TEXT = "EXIT"
+# Versteckter Button Einstellungen
+BUTTON_TEXT = "PAY"
 BUTTON_FARBE = "#0a0a0a" # Fast unsichtbar auf Schwarz
 # ─────────────────────────────────────────────────────
 
